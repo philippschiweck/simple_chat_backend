572e3854fe2cd1645c76203363fc8e186adab74d
Philipp Schiweck <philipp.schiweck@googlemail.com>
2019-06-14 22:24:03 +0200

    Debug Redis Room Publish

--- backend.js
+++ backend.js
@@ -168,9 +168,10 @@ redisSub.on('message', function(channel, JsonData){
 
         } else if (data.newRoom.type === 'private') {
             for(user in data.newRoom.users){
+  
                 let connected = Object.keys(io.sockets.connected);
                 console.log("Keys: " + connected);
-                if(connected.contains(user)){
+                if(connected.contains(user.id)){
                     io.to(user.id).emit('room added', data);
                 }
             }
